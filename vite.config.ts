98408e294ac12c3f797edda0fa0e4b1d4ec50cd3
Nelly R <nretana1089@gmail.com>
2025-06-15 13:34:10 -0600

    build: add env variables to support diff base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { imageToWebpPlugin } from 'vite-plugin-image-to-webp';
@@ -7,55 +7,55 @@ import { imageToWebpPlugin } from 'vite-plugin-image-to-webp';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    imageToWebpPlugin(),
-    //cssInjectedByJsPlugin(),
-    UnpluginInjectPreload({
-      files: [
-        {
-          entryMatch: /.*\.(woff2|.woff)$/,
-          attributes: {
-            type: 'font/woff2',
-            as: 'font',
-            crossorigin: 'anonymous' //include this, otherwise, the preload doesn't work
-          }
-        }
-      ]
-    }),
-    /*visualizer({
+
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [
+      react(),
+      imageToWebpPlugin(),
+      //cssInjectedByJsPlugin(),
+      UnpluginInjectPreload({
+        files: [
+          {
+            entryMatch: /.*\.(woff2|.woff)$/,
+            attributes: {
+              type: 'font/woff2',
+              as: 'font',
+              crossorigin: 'anonymous', //include this, otherwise, the preload doesn't work
+            },
+          },
+        ],
+      }),
+      /*visualizer({
       filename: './dist/bundle-analysis.html', // Output file
       open: true, // Automatically open in browser after build
       gzipSize: true, // Show gzipped sizes
     }),*/
-    ViteImageOptimizer({
-      svg: {
-        plugins: [
-          { name: 'removeViewBox'},
-          { name: 'sortAttrs' },
-        ],
-      }
-    })
-  ],
-  assetsInclude: ['**/*.{jpg,jpeg}'],
-  /* css: {
+      ViteImageOptimizer({
+        svg: {
+          plugins: [{ name: 'removeViewBox' }, { name: 'sortAttrs' }],
+        },
+      }),
+    ],
+    assetsInclude: ['**/*.{jpg,jpeg}'],
+    /* css: {
     modules: {
       generateScopedName: '[name]-[local]-[hash:base64:5]'
     }
   }, */
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, 'src'),
-      '@assets': path.join(__dirname, 'src/assets'),
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src'),
+        '@assets': path.join(__dirname, 'src/assets'),
+      },
     },
-  },
-  build: {
-    sourcemap: false,
-    emptyOutDir: true,
-    /* rollupOptions: {
+    build: {
+      sourcemap: false,
+      emptyOutDir: true,
+      /* rollupOptions: {
       output: {
         manualChunks(id) {
           //create separated files for dependencies
@@ -69,5 +69,7 @@ export default defineConfig({
         },
       },
     }, */
-  },
-});
+    },
+    base: `${env.VITE_APP_BASE_PATH}`,
+  });
+};
